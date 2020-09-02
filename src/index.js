5b49520d651964caf21b01ea99a4371c575d6e63
Stavros Kollias <kollias1252@gmail.com>
2020-09-02 16:35:33 +0100

    custom select component  needs mobile verion

---
+++ src/index.js
@@ -0,0 +1,107 @@
+import _ from 'lodash';
+import "../css/style.less";
+import pcImg from '../images/pc-mixer.jpg';
+
+const optionsObject = { "option 1": "Hello", "option 2": "Stavros", "option 3": "Testing Component" }
+
+
+function component(optionsObject) {
+    // createHtmlElement(type,id,className,innterText,innerHTML,value,placeholder,disabled)
+    const findSelectComponentsDom = document.querySelectorAll("#select-component");
+    const selectComponent = createHtmlElement('button', `select-component-${findSelectComponentsDom.length}`, "select-component", null, null, null, null, null);
+    const textSelectComponent = createHtmlElement('span', null, 'select-text-display', "Select", null, null, null, null);
+    const arrowDown = createHtmlElement('i', null, "fas fa-angle-down", null, null, null, null, null);
+    const selectMenuList = createHtmlElement('ul', "select-menu", "select-menu", null, null, null, null, null);
+    const liSearchElement = createHtmlElement('li', "search-menu-item", "search-menu-item", null, null, null, null, null);
+    const inputSearchDom = document.querySelectorAll("#input-search");
+    const inputSearch = createHtmlElement('input', `input-search ${inputSearchDom.length}`, "input-search", null, null, null, "Search", null);
+    const searchIcon = createHtmlElement('i', null, "fas fa-search", null, null, null, null, null);
+    liSearchElement.appendChild(searchIcon);
+    liSearchElement.appendChild(inputSearch);
+    selectMenuList.appendChild(liSearchElement);
+    addOptionsToSelectionElement(optionsObject, selectMenuList)
+    selectComponent.appendChild(textSelectComponent);
+    selectComponent.appendChild(arrowDown);
+    selectComponent.appendChild(selectMenuList);
+    selectComponent.addEventListener("click", (event) => {
+        handleSelectCompnentClick(event);
+    });
+
+    return selectComponent;
+}
+
+
+
+function addOptionsToSelectionElement(optionsObject, selectionList) {
+    for (const [key, value] of Object.entries(optionsObject)) {
+        const newOption = generateNewOptionItem(key, value, selectionList.children.length - 1);
+        if (selectionList.children.length - 1 == 0) newOption.querySelector(".select-menu-item-button").classList.add("active-item");
+        selectionList.appendChild(newOption);
+    }
+}
+
+function generateNewOptionItem(string, valueOption, childNumber) {
+    const li = createHtmlElement("li", null, "select-menu-item", null, null, null, null, null);
+    const liButton = createHtmlElement("button", null, "select-menu-item-button", null, null, null, null, null);
+    liButton.dataValue = valueOption;
+    liButton.addEventListener("click", (event) => {
+        handleClickSelectionItem(event);
+    });
+    const spanLabel = createHtmlElement("span", `select-menu-item-label-${childNumber}`, "select-menu-item-label", string, null, null, null, null);
+
+    liButton.appendChild(spanLabel);
+    li.appendChild(liButton);
+    return li
+}
+
+function handleSelectCompnentClick(event) {
+    var selectComponent = event.target;
+    !selectComponent.id.includes("select-component") ? selectComponent = event.target.parentElement : selectComponent;
+    if (!selectComponent.id.includes("select-component")) return;
+    const selectMenu = selectComponent.querySelector(".select-menu");
+    openCloseSelecMenu(selectMenu, selectComponent);
+}
+
+
+function openCloseSelecMenu(selectMenu, selectComponent) {
+    if (selectMenu.classList.length == 2) {
+        selectMenu.classList.remove("active-select-menu");
+        var arrow = selectComponent.querySelector(".fa-angle-up");
+        arrow.className = "fa fa-angle-down";
+    } else {
+        selectMenu.classList.add("active-select-menu");
+        var arrow = selectComponent.querySelector(".fa-angle-down");
+        arrow.className = "fa fa-angle-up";
+    }
+}
+
+
+function handleClickSelectionItem(event) {
+    var selectionItem = event.target;
+    !selectionItem.className.includes("select-menu-item-button") ? selectionItem = event.target.parentElement : selectionItem;
+    if (selectionItem == undefined) return;
+    console.log(selectionItem.dataValue);
+    const selectMenu = selectionItem.parentElement.parentElement;
+    const selectComponent = selectMenu.parentElement;
+    selectMenu.querySelector(".active-item").classList.remove("active-item");
+    selectionItem.classList.add("active-item");
+    const labelName = selectionItem.querySelector(".select-menu-item-label").innerText;
+    selectComponent.querySelector(".select-text-display").innerText = labelName;
+    openCloseSelecMenu(selectMenu, selectComponent);
+    //  console.log(`selectItem:${selectionItem.className}`);
+}
+
+
+function createHtmlElement(type, id, className, innerText, innerHTML, value, placeholder, disabled) {
+    const element = document.createElement(type);
+    if (id) element.id = id;
+    if (className) element.className = className;
+    if (innerText) element.innerText = innerText;
+    if (innerHTML) element.innerHTML = innerHTML;
+    if (value) element.value = value;
+    if (placeholder) element.placeholder = placeholder;
+    if (disabled) element.disabled = disabled;
+    return element;
+}
+
+document.body.appendChild(component(optionsObject));
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        // filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.less$/,
+                use: [
+                    {
+                        loader: 'style-loader',
+                    },
+                    {
+                        loader: 'css-loader',
+                    },
+                    {
+                        loader: 'less-loader',
+                        options: {
+                            lessOptions: {
+                                strictMath: true,
+                            },
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader',
+                ],
+            },
+        ],
+    },
+};
